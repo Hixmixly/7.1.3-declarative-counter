2b85eb1f6a86ee65ca7cc26b65661a4f8ddf20ad
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-04-10 22:57:53 -0700

    deleted code in app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit bananas <code>src/App.js</code> and save to reload.
+          I hope i have an easier time with this.
         </p>
         <a
           className="App-link"
